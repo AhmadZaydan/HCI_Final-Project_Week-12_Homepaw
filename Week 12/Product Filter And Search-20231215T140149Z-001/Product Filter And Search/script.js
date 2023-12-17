83246455d8cde10649e142c49eebe159a6bf5bc4
AhmadZaydan <144796105+AhmadZaydan@users.noreply.github.com>
2023-12-17 21:13:47 +0700

    Add files via upload

---
+++ Week 12/Product Filter And Search-20231215T140149Z-001/Product Filter And Search/script.js
@@ -0,0 +1,320 @@
+let pets = {
+  data: [
+    {
+      petName: "Kai",
+      category: "dog",
+      breed: "Akita",
+      image: "images/dog3.jpg",
+      description:
+        "Kai, an Akita dog, sports a striking coat ranging from orange to white. With his sharp eyes and confident demeanor, Kai exudes loyalty and bravery. He's a faithful companion, known for his intelligence, courage, and dedication to his family. Kai has a playful habit of pretending to play dead, adding a charming quirk to his personality.",
+      weight: "2 Kg",
+      age: "6 months",
+      gender: "Male",
+    },
+    {
+      petName: "Snowflake",
+      category: "cat",
+      breed: "Persian",
+      image: "images/cat3.jpg",
+      description:
+        "Snowflake is a Persian cat with plush white fur and sparkling blue eyes. Graceful and charming, her fluffy coat and delicate features exude elegance. Snowflake adores cozy spots and gentle affection. Despite her regal aura, she's an affectionate companion, spreading joy with her gentle purrs.",
+      weight: "4 kg",
+      age: "2 years",
+      gender: "Female",
+    },
+    {
+      petName: "Meigy",
+      category: "others",
+      breed: "GuineaPigs",
+      image: "images/others1.jpg",
+      description:
+        "Meigy, the brown and white Guinea Pig, is an endearing pet with a charming coat. Their sweet nature makes them an ideal companion. Sociable and playful, Meigy enjoys human interaction, bringing joy with their adorable antics and cheerful squeaks. Perfect for anyone seeking a lovable and affectionate furry friend!.",
+      weight: "0,12 Kg",
+      age: "4 Mothns",
+      gender: "Female",
+    },
+    {
+      petName: "Teddy",
+      category: "dog",
+      breed: "Bulldog",
+      image: "images/dog1.jpg",
+      description:
+        "Teddy, a 2-month-old Bulldog, sports a delightful coat with shades of brown and white. Overflowing with energy and affection, Teddy enjoys frolicking with his toys and seeking love from his caring family. His playful charm and loving nature make him an adorable and cherished companion.",
+      weight: "0,7 Kg",
+      age: "2 Months",
+      gender: "Male",
+    },
+    {
+      petName: "Sunny",
+      category: "bird",
+      breed: "Cockatiels",
+      image: "images/bird (1).jpg",
+      description:
+        "Cockatiels are friendly, charming, and sociable birds. With their colorful plumage and distinct crests, they make fantastic companions. They love human interaction, learning tricks, and their joyful chirping adds warmth to any home. Consider adopting one for a lively and affectionate feathered friend!.",
+      weight: "0,085 Kg",
+      age: "1.2 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Sky",
+      category: "others",
+      breed: "White Face",
+      image: "images/others3.jpg",
+      description:
+        "Sky, the vibrant 1.1-year-old male White Face, is an absolute dynamo! Weighing a mere 0.14 Kg, this bundle of energy thrives on leaping about and showcasing his hyperactive tendencies. His boundless enthusiasm and love for frolicking around will surely bring an extra dose of liveliness to any home",
+      weight: "0,14 Kg",
+      age: "1.1 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Luna",
+      category: "cat",
+      breed: "Maine Coon",
+      image: "images/cat1.jpg",
+      description:
+        "Luna is a grey cat with hints of brown in her fur, giving her a captivating appearance. Her golden eyes add allure to her elegant look. She an agile yet calm cat, often basking in sunlight or playing with toys. Luna gentle and friendly nature makes her a warm and comforting companion to everyone around her.",
+      weight: "9 Kg",
+      age: "6 Years",
+      gender: "Female",
+    },
+    {
+      petName: "Azure",
+      category: "bird",
+      breed: "Lovebirds",
+      image: "images/bird (2).jpg",
+      description:
+        "This bird is a small, bright, and colorful bird. It has a grey head, large round white eyes with black pupils, and a pinkish-red beak. Its body transitions from grey at the head to a light blue at the torso, displaying soft and well-groomed feathers. The bird is perched on what appears to be someone's hand or a soft surface.",
+      weight: "0,057 Kg",
+      age: "3.5 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Brownie",
+      category: "others",
+      breed: "Pygmy",
+      image: "images/others.jpg",
+      description:
+        "Brownie is a charming brown Pygmy Rabbit known for its petite size and adorable appearance. With soft, brown fur, Brownie is a delightful addition to any home. Playful and gentle, these rabbits have a curious nature and boundless energy. Their cute demeanor makes them wonderful companions for those seeking an affectionate furry friend!.",
+      weight: "1 Kg",
+      age: "7 Mothns",
+      gender: "Male",
+    },
+    {
+      petName: "Pepper",
+      category: "dog",
+      breed: "Chiuhauha",
+      image: "images/dog2.jpg",
+      description:
+        "Pepper is a Chihuahua with a mix of black, white, and a hint of brown in his coat. He's exceptionally lazy, preferring lounging around over most activities. Often found curled up in cozy spots, Pepper enjoys peaceful relaxation more than anything else. Despite his laziness, he's incredibly endearing and loves receiving gentle affection from his family.",
+      weight: "1 Kg",
+      age: "1.5 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Orion",
+      category: "cat",
+      breed: "BSH",
+      image: "images/cat2.jpg",
+      description:
+        "Orion is a British Shorthair cat with a fading cream coat, exuding a soft and elegant demeanor. His large and captivating eyes shimmer in a deep golden hue, complementing his calm and warm nature. He thoroughly enjoys companionship and cuddles, making him a loving and delightful companion to anyone who knows him.",
+      weight: "5 Kg",
+      age: "3 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Fluffy",
+      category: "others",
+      breed: "Eurasian",
+      image: "images/others4.jpg",
+      description:
+        "Fluffy, the Eurasian cat, is a charming and intelligent feline companion seeking a loving home. With a striking coat of thick fur, exhibiting a blend of captivating patterns and hues, Fluffy stands out among her peers. Her luscious coat not only adds to her aesthetic allure but also reflects her well-groomed and healthy nature. She possesses a pair of mesmerizing, expressive eyes that are sure to melt hearts.",
+      weight: "4.3 Kg",
+      age: "2 Years",
+      gender: "Female",
+    },
+    {
+      petName: "Coco",
+      category: "bird",
+      breed: "Pionus",
+      image: "images/bird (3).jpg",
+      description:
+        "Coco, the male Blue-Headed Pionus, has striking green plumage and a charming blue head. Sociable and intelligent, he's a delightful companion who loves interaction, learning tricks, and serenading with melodious calls. With his affectionate nature, he'll bring joy as a cherished household companion!.",
+      weight: "0,25 Kg",
+      age: "2 Years",
+      gender: "Male",
+    },
+    {
+      petName: "Milky",
+      category: "others",
+      breed: "Micro Pig",
+      image: "images/others2.jpg",
+      description:
+        "Milky, the pink micro pig with charming black patterns, is an adorable pet. With its captivating pink color and cute markings, Milky is an irresistible addition to any home. Despite being small, micro pigs like Milky are curious and cheerful, making them perfect companions for anyone seeking an adorable and sweet pet!.",
+      weight: "2,5 Kg",
+      age: "9.5 Mothns",
+      gender: "Male",
+    },
+    {
+      petName: "",
+      category: "others",
+      breed: "",
+      image: "",
+      description: "",
+      weight: "",
+      age: "",
+      gender: "",
+    },
+  ],
+};
+
+for (let i of pets.data) {
+  let card = document.createElement("div");
+  card.classList.add("card", i.category, "hide");
+
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+
+  let container = document.createElement("div");
+  container.classList.add("container");
+
+  let name = document.createElement("h5");
+  name.classList.add("pet-name");
+  name.innerText = i.petName;
+  name.style.fontWeight = "bold";
+  name.style.fontSize = "1.2em";
+
+  container.appendChild(name);
+
+  let breed = document.createElement("p");
+  breed.innerText = "Breed: " + (i.breed ? i.breed : "Not specified");
+  container.appendChild(breed);
+
+  let info = document.createElement("div");
+  info.classList.add("additional-info");
+
+  let weight = document.createElement("p");
+  weight.innerText = "Weight: " + (i.weight ? i.weight : "Not specified");
+  info.appendChild(weight);
+
+  let age = document.createElement("p");
+  age.innerText = "Age: " + (i.age ? i.age : "Not specified");
+  info.appendChild(age);
+
+  let gender = document.createElement("p");
+  gender.innerText = "Gender: " + (i.gender ? i.gender : "Not specified");
+  info.appendChild(gender);
+
+  container.appendChild(info);
+
+  let clickHereButton = document.createElement("button");
+  clickHereButton.innerText = "More";
+  clickHereButton.classList.add("search-button-style");
+  clickHereButton.style.borderRadius = "30px";
+  clickHereButton.addEventListener("click", redirectToAnotherPage);
+  container.appendChild(clickHereButton);
+
+  card.appendChild(container);
+  document.getElementById("pets").appendChild(card);
+}
+
+function redirectToAnotherPage(event) {
+  let petName =
+    event.currentTarget.parentElement.querySelector(".pet-name").innerText;
+  let selectedPet = pets.data.find(
+    (pet) => pet.petName.toUpperCase() === petName.toUpperCase()
+  );
+
+  if (selectedPet && selectedPet.description) {
+    displayPopup(
+      selectedPet.petName,
+      selectedPet.description,
+      selectedPet.image
+    );
+  } else {
+    displayPopup("Animal not found", "Description not available", "");
+  }
+}
+
+function displayPopup(title, description, imageSrc) {
+  let popupDescription = document.getElementById("popup-description");
+  popupDescription.innerHTML = "";
+
+  let nameHeader = document.createElement("h2");
+  nameHeader.innerHTML = "<strong>" + title + "</strong>";
+  popupDescription.appendChild(nameHeader);
+
+  let descriptionPara = document.createElement("p");
+  descriptionPara.innerHTML = description;
+  popupDescription.appendChild(descriptionPara);
+  document.getElementById("popup-title").innerText = "";
+  document.getElementById("popup-image").setAttribute("src", imageSrc);
+  document.getElementById("popup").style.display = "block";
+
+  let textContainer = document.querySelector(".text-container");
+
+  let adoptLinkExists = document.querySelector(".adopt-link");
+  if (!adoptLinkExists) {
+    let adoptNowLink = document.createElement("a");
+    adoptNowLink.setAttribute("href", "form.html");
+    adoptNowLink.innerText = "Click Here to Adopt";
+    adoptNowLink.classList.add("adopt-link");
+    textContainer.appendChild(adoptNowLink);
+  }
+
+  let closeButton = document.getElementById("close-button");
+  closeButton.style.color = "white";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function filterPet(value) {
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    if (value.toUpperCase() === button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  let elements = document.querySelectorAll(".card");
+  elements.forEach((element) => {
+    if (value === "all") {
+      element.style.display = "block";
+    } else {
+      let category = element.classList[1];
+      if (category === value.toLowerCase()) {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    }
+  });
+}
+
+document.getElementById("search").addEventListener("click", () => {
+  let searchInput = document.getElementById("search-input").value.toUpperCase();
+  let elements = document.querySelectorAll(".card");
+
+  elements.forEach((element) => {
+    let petName = element.querySelector(".pet-name").innerText.toUpperCase();
+    let category = element.classList[1].toUpperCase();
+
+    if (petName.includes(searchInput) || category.includes(searchInput)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
+
+window.onload = () => {
+  filterPet("all");
+};
